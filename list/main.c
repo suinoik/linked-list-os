bd4b58294ff0ec996a537881999ec546ffc103d1
Onwuosiuno Ikhioda <onwuosiuno.ikhioda@bison.howard.edu>
2022-10-14 02:38:06 +0000

    linked list lab

---
+++ list/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "list.h"
+
+int main() 
+{
+  printf("Write your Tests for your linked list implementation\n");
+  
+  list_t *mylist;
+  
+  mylist = list_alloc();
+  
+
+ 
+}
